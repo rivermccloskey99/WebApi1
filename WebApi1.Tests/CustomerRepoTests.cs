a514ae7032eac134d360f3c05eeb53e776a88ab2
River McCloskey <river.mccloskey@pwc.com>
2022-09-09 11:23:30 +0100

    Set up initial test for Customer Repo

--- WebApi1.Tests/CustomerRepoTests.cs
+++ WebApi1.Tests/CustomerRepoTests.cs
@@ -1,3 +1,4 @@
+using System;
 using WebApi1.Data;
 using WebApi1.Models;
 using Xunit;
@@ -10,16 +11,12 @@ namespace WebApi1.Tests
         public void Create_a_null_customer_and_throw_exception()
         {
             // Arrange
-            Customer customer = null;
+            var repo = new CustomerRepo(null);
+            Customer nullCustomer = null;
 
-            // to be mocked
-            CustomerRepo repo = new CustomerRepo();
-
-            // Act
-
-
-            // Assert
-
+            // Act & Assert
+            Assert.Throws<ArgumentNullException>(() => repo.CreateCustomer(nullCustomer));
         }
+
     }
 }
