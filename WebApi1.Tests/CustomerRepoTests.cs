b0961e9cd1540fa00ea0927edc7a77ad8671f4d4
Rory McGrath <rory.m.mcgrath@pwc.com>
2022-09-09 12:23:25 +0100

    Added Delete_a_non_existing_customer_and_throw_exception Test

--- WebApi1.Tests/CustomerRepoTests.cs
+++ WebApi1.Tests/CustomerRepoTests.cs
@@ -1,3 +1,4 @@
+using Moq;
 using System;
 using WebApi1.Data;
 using WebApi1.Models;
@@ -16,6 +17,33 @@ namespace WebApi1.Tests
 
             // Act & Assert
             Assert.Throws<ArgumentNullException>(() => repo.CreateCustomer(nullCustomer));
+        }
+
+        [Fact]
+        public void Delete_a_non_existing_customer_and_throw_exception()
+        {
+            /*
+            
+            // Arrange
+            int id = 1;
+            Mock<AppDbContext> mockContext = new Mock<AppDbContext>();
+            mockContext.Setup(x => x.Customers.Find(id)).Returns<Customer>(null);
+            var repo = new CustomerRepo(mockContext.Object);
+
+            // Assert
+            Assert.Throws<ArgumentNullException>(() => repo.DeleteCustomerById(id));
+
+            */
+
+
+            // Arrange
+            int id = 1;
+            var context = new AppDbContext(null);
+            var repo = new CustomerRepo(context);
+
+            // Assert
+            Assert.Throws<ArgumentNullException>(() => repo.DeleteCustomerById(id));
+
         }
 
     }
